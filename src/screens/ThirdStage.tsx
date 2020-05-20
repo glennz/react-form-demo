4e4e62e6b33cb0a52e034362164e5c0113651107
Glenn <glennzh@gmail.com>
2020-05-21 01:09:50 +1000

    udpate content

--- src/screens/ThirdStage.tsx
+++ src/screens/ThirdStage.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 export default function ThirdStage() {
   return (
-    <div>
-      <h1>Title</h1>
-      <p>blah blah blah blah blah</p>
+    <div className="confirmation-page">
+      <h1>Thank You</h1>
+      <p>
+        Stranded. Yes, she was now the first person ever
+        to land on Venus, but that was of little consequence.
+        Her name would be ready by millions in school
+        as the first to land here, but that celebrity would
+        never actually be seen by her.
+      </p>
     </div>
   );
 }
